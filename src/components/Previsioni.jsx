48a918d0510d83ee3feb8e54d3062bde7be73927
Andrei Briceag <andrei98briceag@gmail.com>
2026-02-13 17:00:50 +0100

    Error in .cath if city 404

--- src/components/Previsioni.jsx
+++ src/components/Previsioni.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import SearchBar from "./SearchBar";
 
 function Previsioni() {
+  const [error, setError] = useState("");
   const [forecast, setForecast] = useState(null);
   const API_KEY = "54a5b511cab0a50f87dcdc9a0a134242";
 
   const fetchForecastByCity = (city) => {
+    setError("");
+    setForecast(null);
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("Città non trovata"); // lancia errore se 404
+        return res.json();
+      })
       .then((data) => setForecast(data))
       .catch((err) => console.error(err));
   };
@@ -16,6 +22,7 @@ function Previsioni() {
     <div className="container my-4 px-5">
       <h2 className="text-center mb-4">Previsioni 5 Giorni</h2>
       <SearchBar onSearch={fetchForecastByCity} />
+      {error && <div className="alert alert-danger mt-3 text-center">{error}</div>}
       {forecast && (
         <div className="mt-4">
           {forecast.list.slice(0, 5).map((item, index) => (
